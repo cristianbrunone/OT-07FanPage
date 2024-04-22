753e3469293db694be7d2497457348070626cd2b
CristianBrunone <140309792+cristianbrunone@users.noreply.github.com>
2024-04-22 20:25:15 -0300

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,24 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    //Obtener los elementos de la clase .close
-    let links = document.querySelectorAll(".close");
+function validafaleConosco(){
+	if (document.frmfaleconosco.txtnome.value==""){
+		alert("Preencha o campo nome.");
+		document.frmfaleconosco.txtnome.focus();
+		return false;
+	}
+	
+	if (document.frmfaleconosco.txtfone.value==""){
+		alert("Preencha o campo telefone.");
+		document.frmfaleconosco.txtfone.focus();
+		return false;
+	}
+	
+	if (document.frmfaleconosco.txtemail.value==""){
+		alert("Preencha o campo e-mail.");
+		document.frmfaleconosco.txtemail.focus();
+		return false;
+	}
 
-    //Recorrerlos
-    links.forEach(function(link){
-        
-        // Agregar un evento a cada uno de ellos - case sensitive
-        link.addEventListener("click", function(ev){
-            ev.preventDefault();
-            let content = document.querySelector('.content');
-
-            //Quitarle la animacion que ya tiene
-            content.classList.remove("animate__fadeInDown"); 
-            content.classList.remove("animate__animated");
-
-            // Agregar clases para animar su salida fadeOutUp
-            content.classList.add("animate__fadeOutUp");
-            content.classList.add("animate__animated");
-
-            setTimeout(function(){
-                location.href ="/boletines";
-            },600);
-
-            return false;
-        });
-    });
-});
-
-
+}
 
 
 
