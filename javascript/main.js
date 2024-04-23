ca409e08b0804c3e1808075b003c6aa8f99577bb
CristianBrunone <140309792+cristianbrunone@users.noreply.github.com>
2024-04-23 19:57:58 -0300

    Agregando Expresiones en Js

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,19 @@
 
 function validafaleConosco(){
-	if (document.frmfaleconosco.txtnome.value==""){
-		alert("Preencha o campo nome.");
+	var nome = document.frmfaleconosco.txtnome.value;
+	var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([ ]{1}[A-zÀ-ü]{2,})+$");
+	
+	if (!expRegNome.test(nome)){
+		alert("Preencha o campo nome corretamente.");
 		document.frmfaleconosco.txtnome.focus();
 		return false;
 	}
 	
-	if (document.frmfaleconosco.txtfone.value==""){
-		alert("Preencha o campo telefone.");
+	var fone = document.frmfaleconosco.txtfone.value;
+	var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+	
+	if (!expRegFone.test(fone)){
+		alert("Preencha o campo telefone corretamente.");
 		document.frmfaleconosco.txtfone.focus();
 		return false;
 	}
